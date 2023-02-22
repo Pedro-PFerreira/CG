fe80bd05ca04b16644674f5105746ebd5359472d
Pedro-CAB <up202006086@up.pt>
2023-02-22 20:10:33 +0000

    Added Blue Triangle [TP2]

--- tp2/MyTangram.js
+++ tp2/MyTangram.js
@@ -2,8 +2,7 @@ import { CGFobject } from '../lib/CGF.js';
 import { MyDiamond } from "./MyDiamond.js";
 import { MyTriangle } from "./MyTriangle.js";
 import { MyParallelogram } from "./MyParallelogram.js";
-import { MyTriangleSmall } from "./MyTriangleSmall.js";
-import { MyTriangleBig } from "./MyTriangleBig.js";
+import { MyTriangleBig } from './MyTriangleBig.js';
 
 /**
  * MyTangram
@@ -15,30 +14,41 @@ export class MyTangram extends CGFobject {
         super(scene);
 
         //Initialize scene objects
-        this.tDiamond = new MyDiamond(this.scene);
-
+        this.square = new MyDiamond(this.scene);
+        this.blueTriangle = new MyTriangleBig(this.scene);
         
     }
     display() {
         var angle = -20;
-        var rotate = [
+        var rotateSquare = [
             Math.cos(angle), - Math.sin(angle), 0, 0,
             Math.sin(angle), Math.cos(angle), 0, 0,
             0, 0, 1, 0,
             0, 0, 0, 1
         ]
 
-        var translate = [
+        var translateSquare = [
             1, 0, 0, 0,
             0, 1, 0, 0,
             0, 0, 1, 0,
             0, 1, 0, 1
             ];
 
+        var translateBlueTriangle = [
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            -0.2, -2.07, 0, 1
+            ];
+
         this.scene.pushMatrix();
-        this.scene.multMatrix(rotate);
-        this.scene.multMatrix(translate);
-        this.tDiamond.display();
+        this.scene.multMatrix(rotateSquare);
+        this.scene.multMatrix(translateSquare);
+        this.square.display();
         this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.multMatrix(translateBlueTriangle);
+        this.blueTriangle.display();
      }
 }
