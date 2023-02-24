946aaf0f54d641bc491e7b64e1a4306262e96394
Pedro-CAB <up202006086@up.pt>
2023-02-24 13:15:59 +0000

    Finished Ex. 2 [TP2]

--- tp2/MyTangram.js
+++ tp2/MyTangram.js
@@ -19,7 +19,9 @@ export class MyTangram extends CGFobject {
         this.blueTriangle = new MyTriangleBig(this.scene);
         this.greenTriangle = new MyTriangleBig(this.scene);
         this.lightBlueTriangle = new MyTriangle(this.scene);
-        
+        this.redTriangle = new MyTriangleSmall(this.scene);
+        this.purpleTriangle = new MyTriangleSmall(this.scene);
+        this.parallelogram = new MyParallelogram(this.scene);
     }
     display() {
         var angle = -20;
@@ -86,7 +88,76 @@ export class MyTangram extends CGFobject {
             0, 0, 1, 0,
             0, 0, 0, 1
         ]
-        
+
+        var translateRedTriangle1 = [
+            1,0,0,0,
+            0,1,0,0,
+            0,0,1,0,
+            0,-0.5,0,1
+        ]
+
+        var translateRedTriangle2 = [
+            1,0,0,0,
+            0,1,0,0,
+            0,0,1,0,
+            0.4,4,0,1
+        ]
+
+        angle = - 90 * (Math.PI / 180);
+        var rotateRedTriangle = [
+            Math.cos(angle), - Math.sin(angle), 0, 0,
+            Math.sin(angle), Math.cos(angle), 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ]
+
+        var translatePurpleTriangle1 = [
+            1,0,0,0,
+            0,1,0,0,
+            0,0,1,0,
+            1,0,0,1
+        ]
+
+        var translatePurpleTriangle2 = [
+            1,0,0,0,
+            0,1,0,0,
+            0,0,1,0,
+            0.9,2.8,0,1
+        ]
+
+        angle = - 150 * (Math.PI / 180);
+        var rotatePurpleTriangle = [
+            Math.cos(angle), - Math.sin(angle), 0, 0,
+            Math.sin(angle), Math.cos(angle), 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ]
+
+        angle = 225 * (Math.PI / 180);
+        var rotateParallelogram = [
+            Math.cos(angle), - Math.sin(angle), 0, 0,
+            Math.sin(angle), Math.cos(angle), 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ]
+        var translateParallelogram1 = [
+            1,0,0,0,
+            0,1,0,0,
+            0,0,1,0,
+            0,-3.5,0,1
+        ]
+        var translateParallelogram2 = [
+            1,0,0,0,
+            0,1,0,0,
+            0,0,1,0,
+            -2,0,0,1
+        ]
+        var reflectParallelogram = [
+            -1,0,0,0,
+            0,1,0,0,
+            0,0,1,0,
+            0,0,0,1
+        ]
 
         this.scene.pushMatrix();
         this.scene.multMatrix(rotateSquare);
@@ -111,6 +182,28 @@ export class MyTangram extends CGFobject {
         this.scene.multMatrix(rotateLightBlueTriangle);
         this.scene.multMatrix(translateLightBlueTriangle);
         this.lightBlueTriangle.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.multMatrix(translateRedTriangle2);
+        this.scene.multMatrix(rotateRedTriangle);
+        this.scene.multMatrix(translateRedTriangle1);
+        this.redTriangle.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.multMatrix(translatePurpleTriangle2);
+        this.scene.multMatrix(rotatePurpleTriangle);
+        this.scene.multMatrix(translatePurpleTriangle1);
+        this.purpleTriangle.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.multMatrix(translateParallelogram2);
+        this.scene.multMatrix(reflectParallelogram);
+        this.scene.multMatrix(translateParallelogram1);
+        this.scene.multMatrix(rotateParallelogram);
+        this.parallelogram.display();
         this.scene.popMatrix();
      }
 }
