aeb44172bb76debdb54df6b64b2ed115d576f178
Pedro Pereira Ferreira <up202004986@edu.fe.up.pt>
2023-03-13 22:22:01 +0000

    Understanding better the prism construction

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -18,6 +18,40 @@ export class MyPrism extends CGFobject {
     }
 
     initBuffers() {
+        
+        this.vertices = [];
+
+        this.indices = [];
+
+        this.normals = [];
+
+
+        var angle = 360 / this.slices;
+
+        for (var i = 0; i <=180; i += angle){
+            this.vertices.push(Math.cos(i));
+            this.vertices.push(0);
+            this.vertices.push(Math.sin(i));
+
+            this.vertices.push(Math.cos(i));
+            this.vertices.push(2);
+            this.vertices.push(Math.sin(i));
+        }
+
+        for (var i = 0; i <= this.slices - 2; i += 2){
+            this.indices.push(i);
+            this.indices.push(i+1);
+            this.indices.push(i+2);
+
+
+            this.indices.push(i);
+            this.indices.push(i+2);
+            this.indices.push(i+1);
+        }
+
+        
+
+/*
 		this.vertices = [
             //Face 1
             -0.5, 2, 0.5,   //0
@@ -53,27 +87,7 @@ export class MyPrism extends CGFobject {
             -1,0,0,         //20
             -1,2,0,         //21
             -0.5, 2, 0.5,   //22
-            -0.5, 0, 0.5    //23
-
-/*
-			0.5, 0, 0.5,   	//1
-            -0.5, 0, 0.5,   //3
-            -1, 0,0,        //4
-            
-            
-
-            0,2,0,          //7
-            0.5, 2, 0.5,   	//8
-
-            -0.5, 2, 0.5,   //10
-            -1, 2,0,        //11
-            
-            
-
-            -0.5, 2, 0.5,   //10
-            -0.5, 0, 0.5,   //3
-            0.5, 0, 0.5,   	//1
-*/        
+            -0.5, 0, 0.5    //23       
 		];
 
 		//Counter-clockwise reference of vertices
@@ -162,7 +176,7 @@ export class MyPrism extends CGFobject {
             -1,0,1
 
         ]
-
+*/
 		//The defined indices (and corresponding vertices)
 		//will be read in groups of three to draw triangles
 		this.primitiveType = this.scene.gl.TRIANGLES;
--- tp3/MyScene.js
+++ tp3/MyScene.js
@@ -35,7 +35,7 @@ export class MyScene extends CGFscene {
         this.pyramid = new MyPyramid(this, 3, 1);
         this.tangram = new MyTangram(this);
         this.myUnitCube = new MyUnitCube(this);
-        this.prism = new MyPrism(this);
+        this.prism = new MyPrism(this, 6, 1);
         
         this.objects = [this.plane, this.pyramid, this.cone, this.tangram, this.myUnitCube, this.prism];
 
