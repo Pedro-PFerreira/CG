86a9b3f7b14ee00087d7fce4aaacaadcf83fee2c
Pedro Pereira Ferreira <up202004986@edu.fe.up.pt>
2023-03-14 10:06:28 +0000

    corrected tp3-2

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -1,99 +1,71 @@
 import {CGFobject} from '../lib/CGF.js';
 /**
- * MyPrism
+ * MyDiamond
  * @constructor
  * @param scene - Reference to MyScene object
  */
 export class MyPrism extends CGFobject {
-    constructor(scene, slices, stacks){
-
-        super(scene);
+	constructor(scene, slices, stacks) {
+		super(scene);
 
         this.slices = slices;
-
         this.stacks = stacks;
 
-        this.initBuffers();
-
-    }
-
-    initBuffers() {
-        
-        this.vertices = [];
-
+		this.initBuffers();
+	}
+	
+	initBuffers() {
+		this.vertices = [];
         this.indices = [];
-
         this.normals = [];
 
-
         var angle = 0;
+        var var_inc = 2*Math.PI/this.slices;
 
         for (var i = 0; i < this.stacks; i++){
-            
-            
-            var norm_angle = angle/2; //Usam a mesma normal por face, logo pode ser o ângulo médio
+            var normalanglele = var_inc/2;
+            for(var j = 0; j < this.slices; j++){
 
-            for (var j = 0; j < this.slices; j++){
+                var height = 1;
 
-                var h = 1;
+                var sin_a=Math.sin(angle);
+                var sin_aa=Math.sin(angle+var_inc);
+                var cos_a=Math.cos(angle);
+                var cos_aa=Math.cos(angle+var_inc);
 
-                //Enviar as coordenadas de cada vértice. O ângulo aumenta 360º a dividir pelo número de faces, devido à inscrição no cilindro.
-                this.vertices.push(Math.cos(angle), Math.sin(angle + (2*Math.PI / this.slices)), i*h);
-                this.vertices.push(Math.cos(angle + (2*Math.PI / this.slices)), Math.sin(angle), (i+1)*h);
-                this.vertices.push(Math.cos(angle), Math.sin(angle), (i+1)*h);
-                this.vertices.push(Math.cos(angle + (2*Math.PI / this.slices)), Math.sin(angle + (2*Math.PI / this.slices)), (i+1)*h);
+                this.vertices.push(cos_a, sin_a, i*height);
+                this.vertices.push(cos_aa, sin_aa, (i+1)*height);
+                this.vertices.push(cos_a, sin_a, (i+1)*height);
+                this.vertices.push(cos_aa, sin_aa, i*height);
 
+                this.normals.push(Math.cos(normalanglele), Math.sin(normalanglele), 0);
+                this.normals.push(Math.cos(normalanglele), Math.sin(normalanglele), 0);
+                this.normals.push(Math.cos(normalanglele), Math.sin(normalanglele), 0);
+                this.normals.push(Math.cos(normalanglele), Math.sin(normalanglele), 0);
 
-                //Enviar os vetores normais
-                this.normals.push(Math.cos(norm_angle), Math.sin(norm_angle), 0);
-                this.normals.push(Math.cos(norm_angle), Math.sin(norm_angle), 0);
-                this.normals.push(Math.cos(norm_angle), Math.sin(norm_angle), 0);
-                this.normals.push(Math.cos(norm_angle), Math.sin(norm_angle), 0);
-
-                //Enviar os índices. Valores múltiplos com números de quatro (j*4), pois as faces são retangulares (4 vértices)
                 this.indices.push((j*4+1)+(this.slices*4*i), (j*4+2)+(this.slices*4*i), (j*4+0)+(this.slices*4*i));
                 this.indices.push((j*4+0)+(this.slices*4*i), (j*4+3)+(this.slices*4*i), (j*4+1)+(this.slices*4*i));
 
 
+                angle += var_inc;
+                normalanglele += var_inc;
             }
-            
-            angle += (2*Math.PI / this.slices);
-            norm_angle += (2*Math.PI / this.slices);
-
         }
 
-
-
-/*
-        for (var i = 0; i <=2*Math.PI; i += angle){
-            this.vertices.push(Math.cos(i));
-            this.vertices.push(0);
-            this.vertices.push(Math.sin(i));
-
-            this.vertices.push(Math.cos(i));
-            this.vertices.push(2);
-            this.vertices.push(Math.sin(i));
-        }
-
-        for (var i = 0; i <= this.slices - 2; i += 2){
-            this.indices.push(i);
-            this.indices.push(i+1);
-            this.indices.push(i+2);
-
-
-            this.indices.push(i);
-            this.indices.push(i+2);
-            this.indices.push(i+1);
-        }
-*/
 		//The defined indices (and corresponding vertices)
-		//will be read in groups of three to draw triangles
+		//will be read in groups of three to draw triangleles
 		this.primitiveType = this.scene.gl.TRIANGLES;
 
 		this.initGLBuffers();
 	}
+    /**
+     * Called when user interacts with GUI to change object's complexity.
+     * @param {integer} complexity - changes number of slices
+    */ 
+    updateBuffers(complexity){
+        this.slices = 4 + Math.round(8 * complexity); //complexity varies 0-1, so slices varies 3-12
 
-    updateBuffers(complextity){
+        // reinitialize buffers
         this.initBuffers();
         this.initNormalVizBuffers();
     }
--- tp3/MyScene.js
+++ tp3/MyScene.js
@@ -36,7 +36,7 @@ export class MyScene extends CGFscene {
         this.pyramid = new MyPyramid(this, 3, 1);
         this.tangram = new MyTangram(this);
         this.myUnitCube = new MyUnitCube(this);
-        this.prism = new MyPrism(this, 8, 20);
+        this.prism = new MyPrism(this, 8, 1);
         this.cylinder = new MyCylinder(this);
         
         this.objects = [this.plane, this.pyramid, this.cone, this.tangram, this.myUnitCube, this.prism, this.cylinder];
@@ -196,9 +196,19 @@ export class MyScene extends CGFscene {
             this.objects[this.selectedObject].enableNormalViz();
         else
             this.objects[this.selectedObject].disableNormalViz();
+
+
+        if (this.selectedObject == 5){
+            this.pushMatrix();
+            this.rotate(-Math.PI/2, 1, 0, 0);
+            this.objects[this.selectedObject].display();
+            this.popMatrix();
+        }
+        else{
+           this.objects[this.selectedObject].display();
+           this.popMatrix(); 
+        }
         
-        this.objects[this.selectedObject].display();
-        this.popMatrix();
         // ---- END Primitive drawing section
     }
 }
