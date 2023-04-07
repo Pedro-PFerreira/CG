733850dd14ad0dd71b6e461f89e2abb5939d5701
Pedro Pereira Ferreira <up202004986@edu.fe.up.pt>
2023-04-07 16:17:38 +0100

    Created Sphere

--- project/MySphere.js
+++ project/MySphere.js
@@ -3,6 +3,8 @@ import {CGFobject} from '../lib/CGF.js';
  * MySphere
  * @constructor
  * @param scene - Reference to MyScene object
+ * @param slices - Reference to the number of slices
+ * @param stacks - Reference to the number of stacks
  */
 export class MySphere extends CGFobject {
 	constructor(scene, slices, stacks) {
@@ -20,40 +22,50 @@ export class MySphere extends CGFobject {
         this.normals = [];
         this.textCoords = [];
 
-        var index = 0;
+        var latDivs = 2*this.stacks;
+
         var theta = 0;
         var alpha = 0;
-        var theta_inc = Math.PI / 2*this.stacks;
-        var alpha_inc = (2*Math.PI) / this.slices;
+        var theta_inc = Math.PI / latDivs;
+        var alpha_inc = (2*Math.PI) / this.slices;  
         var vertices_inc = this.slices +1;
 
-        for (var i = 0; i <= (2*this.stacks); i++){
+        for (var i = 0; i <= latDivs; i++){
             var sin_theta = Math.sin(theta);
-            var cos_theta = Math.cos(alpha);
+            var cos_theta = Math.cos(theta);
 
             alpha = 0;
             for (var j = 0; j <= this.slices; j++){
-                this.vertices.push(Math.cos(alpha)* sin_theta);
-                this.vertices.push(cos_theta);
-                this.vertices.push(Math.cos(-alpha)* sin_theta);
-                
-                this.normals.push(Math.cos(alpha)* sin_theta);
-                this.normals.push(cos_theta);
-                this.normals.push(Math.cos(-alpha)* sin_theta);
+                var x = Math.cos(alpha)* sin_theta;
+                var y = cos_theta;
+                var z = Math.sin(-alpha)* sin_theta;
 
+                this.vertices.push(x);
+                this.vertices.push(y);
+                this.vertices.push(z);
 
-                if (j < (this.stacks * 2) && (i < this.slices)){
-                    this.indices.push(j*vertices_inc+ i + 1);
-                    this.indices.push(j*vertices_inc+ i);
-                    this.indices.push(j*vertices_inc+ i+ this.slices + 1);                    
+                if (i < latDivs && j < this.slices){
+                    var current = i * vertices_inc + j;
+                    var next = current + vertices_inc;
+ 
+                    this.indices.push(current + 1);
+                    this.indices.push(current);
+                    this.indices.push(next);
+                    
+                    this.indices.push(current + 1);
+                    this.indices.push(next);
+                    this.indices.push(next + 1);
                 }
 
-                this.textCoords.push(i/(this.slices), j/(this.stacks*2));
-                
+                this.normals.push(x);
+                this.normals.push(y);
+                this.normals.push(z);
+
                 alpha += alpha_inc
 
-            }
+                this.textCoords.push(j/(this.slices), i/latDivs);
 
+            }
             theta += theta_inc;
         }
 
