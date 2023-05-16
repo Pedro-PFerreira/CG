c2b2685b3a9dbb589d807e9c4ec9a9511d640ea2
Pedro-CAB <up202006086@up.pt>
2023-05-16 10:05:20 +0100

    New Wing Animation

--- project/MyWing.js
+++ project/MyWing.js
@@ -23,10 +23,10 @@ export class MyWing extends CGFobject{
         this.scene.pushMatrix();
         this.scene.translate(-1,0,0);
         this.scene.pushMatrix();
-        this.scene.translate(3 - 2 * this.animVal - Math.sin(this.animVal * Math.PI/6),-1,0);
+        this.scene.translate(3.6 - 3 * this.animVal - Math.sin(this.animVal * Math.PI/6),-1.05 + this.animVal * 0.001,0);
         this.scene.rotate(Math.PI/2, 1, 0, 0);
         //this.scene.rotate(-Math.PI/6, 0,1,0);
-        this.scene.rotate(-Math.PI/6 * this.animVal * 5, 0,1,0);
+        this.scene.rotate(-Math.PI/6 * this.animVal * 4.8, 0,1,0);
         this.scene.scale(1.5,1,1);
         this.wingBorder.display();
         this.scene.popMatrix();
@@ -36,7 +36,7 @@ export class MyWing extends CGFobject{
         console.log(this.animVal);
         this.scene.rotate(Math.PI/2, 1, 0, 0);
         //this.scene.rotate(Math.PI/6, 0,1,0);
-        this.scene.rotate(Math.PI/6 * this.animVal * 5, 0,1,0);
+        this.scene.rotate(Math.PI/6 * this.animVal * 4.5, 0,1,0);
         this.scene.scale(1.5,1,1);
         this.wingMiddle.display();
         this.scene.popMatrix();
